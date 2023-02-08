0f88f53f7c26682aef42d6ae7b44af69c754926e
julio <julioarrud@gmail.com>
2023-02-08 16:21:41 -0300

    hastily added a template to use with index.php

--- index.php
+++ index.php
@@ -1,14 +1,46 @@
 <?php
   require_once 'conn.php';
   ?>
-<html>
-<head>
-  <title>CRUD Application</title>
-</head>
-<body>
-  <h1>CRUD Application</h1>
-  <br>
-  <a href="display_entries.php">View Entries</a>
-  <a href="form.html">add entry</a>
-</body>
-</html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>simples crud</title>
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container px-lg-5">
+                <a class="navbar-brand" href="#!"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!"><!-- Home --></a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!"><!-- About --></a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!"><!-- Contact --></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="py-5">
+            <div class="container px-lg-5">
+                <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
+                    <div class="m-4 m-lg-5">
+                        <h1 class="display-5 fw-bold">CRUD</h1>
+                        <p class="fs-4">Basic CRUD functions</p>
+                        <a class="btn btn-primary btn-lg" href="form.html">Add Entry</a>
+                        <a class="btn btn-primary btn-lg" href="display_entries.php">View Entries</a>
+                        
+                    </div>
+                </div>
+            </div>
+        </header>
+      </body
+
